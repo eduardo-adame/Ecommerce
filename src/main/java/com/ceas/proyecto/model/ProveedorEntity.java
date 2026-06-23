38d03f8afb16ab664242e0e2ac9fe2aa399eff66
Eduardo <cesaradame624@gmail.com>
2026-06-23 12:08:34 -0600

    Agregue entidad proveedor

---
+++ src/main/java/com/ceas/proyecto/model/ProveedorEntity.java
@@ -0,0 +1,37 @@
+package com.ceas.proyecto.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "proveedor")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProveedorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NonNull
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = false, length = 500)
+    private String descripcion;
+
+    @Column(nullable = false, length = 100)
+    private String direccion;
+
+    @Column(nullable = false, length = 100)
+    private String telefono;
+
+    @Column(nullable = false, length = 100)
+    private String correo;
+}
